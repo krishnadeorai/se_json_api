5626463dfc5fcf136e0d6b0d65873bb6fe5bf3c4
Krishna <krishna.b@caratlane.com>
2017-12-28 02:00:44 +0530

    redirect root to api/docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   jsonapi_resources :products
+
+  root :to => redirect('/api/docs')
+
   get '/api/docs', to: 'apidocs#index'
   get '/simulate/*path', to: 'apidocs#simulate'
   get '/*path', to: 'apidocs#show'
+
+
 end
